0947477f6340531822d460b952029f119714e3fb
Pierre Radermecker <pradermecker@cirb.brussels>
2017-05-05 10:44:23 +0200

    Add updateNixpkgs zsh func

--- .zsh_custom/fun.zsh
+++ .zsh_custom/fun.zsh
@@ -5,3 +5,7 @@ function sshi () {
 function ssrc () {
   git rev-list --all | GIT_PAGER=less xargs git grep "$1"
 }
+
+function updateNixpkgs () {
+    nix-prefetch-git https://github.com/NixOS/nixpkgs.git "$1" > ~/.config/nixpkgs/.nixpkgs.json
+}
