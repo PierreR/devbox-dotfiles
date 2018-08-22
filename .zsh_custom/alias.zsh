390d47efe98fc6f8cbaf6c72d3c9e0973324f315
Pierre Radermecker <pradermecker@cirb.brussels>
2018-08-22 18:27:33 +0200

    Update zsh script

--- .zsh_custom/alias.zsh
+++ .zsh_custom/alias.zsh
@@ -1,2 +1,4 @@
 alias updateCICDShell='nix-env -f ~/.config/nixpkgs/pin.nix -iA pkgs.cicd-shell'
 alias dev-ghc-env='nix-env -f ~/.dev/dev-ghc-env.nix -i dev-ghc-env'
+alias scpi='scp -i ~/.ssh/cirb_rsa'
+alias vpn-cirb='sudo openfortivpn 195.244.171.1:443 -u pradermecker --trusted-cert d8124328c0989459970b5f5d196057902949056c4463e8f5a92961e35691a2d1';
--- .zsh_custom/fun.zsh
+++ .zsh_custom/fun.zsh
@@ -2,6 +2,23 @@ function ssrc () {
     git rev-list --all | GIT_PAGER=less xargs git grep "$1"
 }
 
-function updateNixpkgs () {
-    nix-prefetch-git https://github.com/NixOS/nixpkgs.git "$1" > ~/.config/nixpkgs/.nixpkgs.json
+function updatenixpkgs () {
+    nix-prefetch-git https://github.com/nixos/nixpkgs.git "$1" > ~/.config/nixpkgs/.nixpkgs.json
+}
+
+function publishDevbox () {
+    if [ -z "$1" ]
+    then
+        echo "Please specify a version to publish";
+    else
+        local boxFile="/vagrant/devbox-${1}.box"
+        local target="svifscapl003.prd.srv.cirb.lan:/tmp"
+        if [  -f $boxFile ]
+        then
+            echo "Publishing ${boxFile} to ${target}"
+            scp -i ~/.ssh/cirb_rsa $boxFile pradermecker@$target
+        else
+            echo "No $boxFile found!";
+        fi
+    fi
 }
